7fca7e03487b744bc46ab1bce21cb4fbcb96a176
Doug Jones <DSJR1284@GMAIL.COM>
2021-08-28 06:10:49 -0400

    create action files

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,14 +3,15 @@ import { Switch, Route} from 'react-router-dom'
 
 import Hub from './Hub'
 import About from './About'
+// import Logout from './logout'
 
 
 const Router = () => {
     return(
     <Switch>
-        {/* <Route exact path='/About' /> */}
         <Route exact path='/hub' component={Hub}/>
         <Route exact path='/about' component={About}/>
+        {/* <Route exact path='/logout' component={Logout}/> */}
     </Switch>
     )
 }
